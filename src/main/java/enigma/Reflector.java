1c1644587b72149af5dba27ece567aa7be4738b3
Ricotimes <serveurrp21@gmail.com>
2020-01-13 11:31:51 +0100

    Commentaire de la classe reflector
     Changes to be committed:
    	modified:   src/main/java/enigma/Reflector.java

--- src/main/java/enigma/Reflector.java
+++ src/main/java/enigma/Reflector.java
@@ -4,24 +4,24 @@ package enigma;
  *  @author
  */
 public class Reflector extends Rotor {
-	
+
 	int[] reflection;
 	
 	public static Reflector reflectorFactory(String str){
-		char[] s = str.trim().replace(" ", "").toCharArray();
+		char[] s = str.trim().replace(" ", "").toCharArray();  //We remove all things which are not letters
 		int[] cipher = new int[26];
-		for (int i = 0; i< 26; i++){
+		for (int i = 0; i< 26; i++){											  //Permiss to work with ints between 0 and 26
 			cipher[i] = toIndex(s[i]);
 		}
-		return new Reflector(cipher);
+		return new Reflector(cipher);											  //Creation of the object
 	}
 	
-	Reflector(int[] r){
+	Reflector(int[] r){   // constructor of the reflector
 		super(r,0);
 		reflection = r;
 	}
         
-    public int convertForward(int p) {
+    public int convertForward(int p) {						// We need to refactor this
         return ((reflection[((p)%26+26)%26])%26+26)%26;
     }
 

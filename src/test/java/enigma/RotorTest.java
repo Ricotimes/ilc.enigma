b9e026e4dde8d8bc561a3d6a950ac484a63357dc
Ricotimes <serveurrp21@gmail.com>
2020-01-13 12:05:08 +0100

    Test des fonctions convert For/backward  Changes to be committed: 	modified:   src/test/java/enigma/RotorTest.java

--- src/test/java/enigma/RotorTest.java
+++ src/test/java/enigma/RotorTest.java
@@ -6,9 +6,24 @@ import org.junit.Test;
 
 public class RotorTest {
 
+
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void aRotor1convertBackward1(){
+		Rotor rotor = Rotor.rotorFactory("E K M F L G D Q V Z N T O W Y H X U S P A I B R C J", "Q");
+		rotor.setPosition(25);
+		int data = rotor.convertBackward(2);
+		int expected =23;
+		assertEquals(expected,data);
+	}
+
+	@Test
+	public void aRotor1convertForward1(){
+		Rotor rotor = Rotor.rotorFactory("E K M F L G D Q V Z N T O W Y H X U S P A I B R C J", "Q");
+		rotor.setPosition(25);
+		int data = rotor.convertForward(2);
+		int expected =11;
+		assertEquals(expected,data);
 	}
 
 }

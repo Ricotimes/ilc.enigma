735987daf9ba21644986edeb89f8b93be1bb71b3
a.jaugey@gmail.com <>
2020-01-13 11:58:44 +0100

    Rotor class improved (convertForward/Backward refactored & use of constant)

--- src/main/java/enigma/Rotor.java
+++ src/main/java/enigma/Rotor.java
@@ -2,9 +2,11 @@ package enigma;
 
 public class Rotor {
 
+    private static int ALPHABET_SIZE = 26;
+    
     private int position;
-    private int[] cipher = new int[26];
-    private int[] bcipher = new int[26];
+    private int[] cipher = new int[ALPHABET_SIZE];
+    private int[] bcipher = new int[ALPHABET_SIZE];
     private int notch1 = -1;
     private int notch2 = -1;
 
@@ -18,8 +20,8 @@ public class Rotor {
     
     public static Rotor rotorFactory(String str, String notches){
             char[] s = str.trim().replace(" ", "").toCharArray();           //On retire les espaces et caractères spéciaux de formattage
-            int[] cipher = new int[26];
-            for (int i = 0; i< 26; i++){                                    //On récupère un tableau avec les index des lettres dans l'alphabet
+            int[] cipher = new int[ALPHABET_SIZE];
+            for (int i = 0; i< ALPHABET_SIZE; i++){                                    //On récupère un tableau avec les index des lettres dans l'alphabet
                     cipher[i] = toIndex(s[i]);                              //A l'indice i, on a l'index de la i-ième lettre
             }                                                               
             s = notches.trim().replace(" and ", "").toCharArray();          //On récupère juste les lettres dans un tableau
@@ -45,15 +47,15 @@ public class Rotor {
     }
 
     public int convertForward(int p) {
-        return ((cipher[((p + position) % 26 + 26) % 26] - position) %26 + 26) % 26;           
+        return (cipher[(p + position) % ALPHABET_SIZE] - position) %ALPHABET_SIZE;           
     }
 
     public int convertBackward(int e) {
-        return ((bcipher[((e + position) % 26 + 26) % 26] - position) %26 + 26) % 26;
+        return (bcipher[(e + position) % ALPHABET_SIZE] - position) %ALPHABET_SIZE;
     }
     
     public void advance() {
-        position = (position+1) % 26;
+        position = (position+1) % ALPHABET_SIZE;
     }
     
     protected boolean atNotch() {
@@ -69,7 +71,7 @@ public class Rotor {
     }
     
 	private void createBCipher() {
-		for(int i =0; i<26; i++)
+		for(int i =0; i<ALPHABET_SIZE; i++)
 			bcipher[cipher[i]] = i;
 	}
 
